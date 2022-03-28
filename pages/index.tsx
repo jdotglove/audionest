0b2a5629e71aa3474e47064ec38c2dd9e1a8db86
jdotglove <glover.jarod@gmail.com>
2022-03-28 01:28:16 -0400

    ANEST-5: checking for heroku url

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
-const { SpotifyAuth, Scopes } = require('react-spotify-auth');
+//const { SpotifyAuth, Scopes } = require('react-spotify-auth');
 
 export default function Home() {
   return (
