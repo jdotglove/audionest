1057f837922c130436aa54a6fc3bab053a53343f
jdotglove <glover.jarod@gmail.com>
2022-05-06 16:54:43 -0400

    ANEST-10: setup lru cache and work it into track stats and playlist tracks logic

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,6 +32,7 @@ module.exports = {
         'react/react-in-jsx-scope': 'off',
         'react/prop-types': 'off',
         'react/require-default-props': 'off',
+        ''
       },
       settings: {
         "react": {
--- src/components/Containers/PlaylistDetails.tsx
+++ src/components/Containers/PlaylistDetails.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Card, Col, ListGroup, Row } from 'react-bootstrap';
+import { Container, Card, Col, ListGroup, Row, Button } from 'react-bootstrap';
 import SpotifyContext from '../../contexts/SpotifyContext';
 import styles from '../../../styles/StatisticsSection.module.css';
 import TrackSelector from '../Buttons/TrackSelector';
@@ -14,20 +14,22 @@ export default function PlaylistDetails() {
               <Col>
                 <h3>Selected Playlist Details:</h3>
                 <Card style={{ width: '18rem' }}>
-                  <Card.Header className={styles.playlistTrackContainerHeader}>
+                  <Card.Header
+                    className={styles['playlist-track-container-header']}
+                  >
                     <h5> {currentSelectedPlaylist.name} </h5>
+                    <Button>Show Full Playlist Breakdown</Button>
                   </Card.Header>
                   <Card.Body>
-                  {/* <p>{ JSON.stringify(currentSelectedPlaylist.tracks[0].track, null, 4) }</p> */}
-                        <ListGroup>
-                          {currentSelectedPlaylist.tracks.map(
-                            (
-                              { track },
-                            ) => (
-                              <TrackSelector key={track.id} track={track} setSelectedTracks={setSelectedTracks} />
-                            ),
-                          )}
-                        </ListGroup>
+                    <ListGroup className={styles['playlist-track-group']}>
+                      {currentSelectedPlaylist.tracks.map(({ track }) => (
+                        <TrackSelector
+                          key={track.id}
+                          track={track}
+                          setSelectedTracks={setSelectedTracks}
+                        />
+                      ))}
+                    </ListGroup>
                   </Card.Body>
                 </Card>
               </Col>
