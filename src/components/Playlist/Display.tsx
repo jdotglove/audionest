da1518ff75076293a18cbd368f680fa9d22bd4b1
jdotglove <glover.jarod@gmail.com>
2023-08-11 18:23:56 -0400

    ANEST-15: fix remaining merge conflict

--- src/components/Playlist/Display.tsx
+++ src/components/Playlist/Display.tsx
@@ -2,11 +2,7 @@ import React, { Fragment } from "react";
 
 import SpotifyContext from "../../contexts/SpotifyContext";
 import PlaylistSelector from "../Buttons/PlaylistSelector";
-<<<<<<< HEAD
 import { Container, Card, ListGroup } from "react-bootstrap";
-=======
-import { Container } from "react-bootstrap";
->>>>>>> 5e972f4776011fa5e1cdd0b878045f71bda9da7c
 import PlaylistProvider from "../../providers/PlaylistProvider";
 
 export default function PlaylistDisplay() {
@@ -15,7 +11,6 @@ export default function PlaylistDisplay() {
       {({ playlists, setSelectedPlaylist }) => (
         <Container>
           {playlists.length && (
-<<<<<<< HEAD
             <Card className="d-grid m-3 gap-2" style={{ height: "50rem", maxWidth: "300px" }}>
                 <h3>User Playlists</h3>
               <Card.Body className="overflow-scroll">
@@ -35,23 +30,6 @@ export default function PlaylistDisplay() {
                 </ListGroup>
               </Card.Body>
             </Card>
-=======
-            <div className="d-grid m-3 gap-2" style={{ maxWidth: "300px" }}>
-              <h3>Let&#39;s see what we&#39;re working with...</h3>
-              {playlists.map((playlist: Audionest.Playlist, idx: number) => {
-                return (
-                  <Fragment key={idx}>
-                    <PlaylistProvider>
-                      <PlaylistSelector
-                        playlist={playlist}
-                        setSelectedPlaylist={setSelectedPlaylist}
-                      />
-                    </PlaylistProvider>
-                  </Fragment>
-                );
-              })}
-            </div>
->>>>>>> 5e972f4776011fa5e1cdd0b878045f71bda9da7c
           )}
         </Container>
       )}
